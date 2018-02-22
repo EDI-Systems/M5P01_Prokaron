855fe72973648fb54ad3ce734477de08ed6d7b41
EDI Systems <644580703@qq.com>
2018-02-22 18:27:37 -0500

    cleaned up

---
+++ Project/RVMDK-STM32F767IGT6-RVM/copy.bat
@@ -0,0 +1,3 @@
+copy platform_cmx_RVM_conf.h ..\..\MProkaron\Include\Platform\CortexM_RVM\platform_cmx_RVM_conf.h
+copy RMP_platform.h ..\..\MProkaron\Include\Platform\RMP_platform.h
+copy test.h ..\..\MProkaron\Benchmark\test.h
