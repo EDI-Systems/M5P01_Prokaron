04cc7e18820c01ba867ca5338849835f4aee7623
EDI Systems <644580703@qq.com>
2018-02-27 22:58:03 -0500

    :checkered_flag: Cortex-M0 mature support added

--- MProkaron/Include/Platform/CortexM/platform_cmx_conf.h
+++ MProkaron/Include/Platform/CortexM/platform_cmx_conf.h
@@ -7,7 +7,7 @@ Description: The configuration file for Cortex-M HAL.
 ******************************************************************************/
 
 /* Config Includes ***********************************************************/
-#include "Platform/CortexM/Chips/STM32L053C8/platform_STM32L053C8.h"
+#include "Platform/CortexM/Chips/STM32F405RG/platform_STM32F405RG.h"
 /* End Config Includes *******************************************************/
 
 /* End Of File ***************************************************************/
