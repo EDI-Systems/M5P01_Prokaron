7c5adb4f770b7ba1cf8cf473b533f2c780fefdbd
panrunyu <panrunyu@gwu.edu>
2018-03-01 10:41:59 -0500

    :white_check_mark:Mature travis-ci integration

---
+++ Project/CCS-MSP430FR5994/RMP/Debug/subdir_vars.mk
@@ -0,0 +1,11 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5994.cmd 
+
+
