16a64211fd80cee7bff02d6b79a6fe4ec9311bb9
panrunyu <panrunyu@gwu.edu>
2018-03-01 10:37:46 -0500

    :white_check_mark:Mature travis-ci integration

---
+++ Project/CCS-MSP430FR5994/RMP/Debug/Kernel/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+F:/Code_Library/MCU/Mutatus/M5P1_MuProkaron/MProkaron/Kernel/kernel.c 
+
+C_DEPS += \
+./Kernel/kernel.d 
+
+OBJS += \
+./Kernel/kernel.obj 
+
+OBJS__QUOTED += \
+"Kernel\kernel.obj" 
+
+C_DEPS__QUOTED += \
+"Kernel\kernel.d" 
+
+C_SRCS__QUOTED += \
+"F:/Code_Library/MCU/Mutatus/M5P1_MuProkaron/MProkaron/Kernel/kernel.c" 
+
+
---
+++ Project/CCS-MSP430FR5994/RMP/Debug/main.d
@@ -0,0 +1,17 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/msp430fr5994.h
+main.obj: D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: D:/Program_Files_x86/TI/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
+main.obj: D:/Program_Files_x86/TI/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
+main.obj: D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/legacy.h
+
+../main.c: 
+D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/msp430.h: 
+D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/msp430fr5994.h: 
+D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/in430.h: 
+D:/Program_Files_x86/TI/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
+D:/Program_Files_x86/TI/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
+D:/Program_Files_x86/TI/ccs740/ccsv7/ccs_base/msp430/include/legacy.h: 
