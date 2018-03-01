4d0109bdbe302a14b6a8ee51f7b2d6971d9dcc84
panrunyu <panrunyu@gwu.edu>
2018-03-01 11:01:54 -0500

    :white_check_mark:Mature travis-ci integration

--- Project/ECLIPSE-GCC-LINUX/RMP/copy.sh
+++ Project/ECLIPSE-GCC-LINUX/RMP/copy.sh
@@ -1,3 +1,3 @@
 cd ..
-cp -f RMP_platform.h ../../MProkaron/Include/Platform/RMP_platform.h
-cp -f test.h ../../MProkaron/Benchmark/test.h
+cp -f RMP_platform.h ../../../MProkaron/Include/Platform/RMP_platform.h
+cp -f test.h ../../../MProkaron/Benchmark/test.h
