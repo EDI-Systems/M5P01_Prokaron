576cd6d1713d7ae32e372029ba1c8658a41a1b56
EDI Systems <644580703@qq.com>
2018-05-02 22:53:53 -0400

    :racehorse:performance boost for Cortex-M series

--- MProkaron/Platform/CortexM/platform_cm0_asm.s
+++ MProkaron/Platform/CortexM/platform_cm0_asm.s
@@ -131,8 +131,6 @@ ONE             CHECK_BITS      1
 ;Output      : None.                                      
 ;*****************************************************************************/
 _RMP_Yield
-                PUSH            {R0-R1}
-                
                 LDR             R0,=0xE000ED04         ;The NVIC_INT_CTRL register
                 LDR             R1,=0x10000000         ;Trigger the PendSV          
                 STR             R1,[R0]
@@ -140,7 +138,6 @@ _RMP_Yield
                 DSB                                    ;Data and instruction barrier
                 ISB
                 
-                POP             {R0-R1}                
                 BX              LR                                                   
 ;/* End Function:_RMP_Yield **************************************************/
 
