ed9e1383583bb0617972bdd00442db9a703b54de
EDI Systems <644580703@qq.com>
2018-07-19 18:56:10 -0400

    :fire:delete unused files

---
+++ MProkaron/Include/Platform/RV32IMAC/rmp_platform_rv32imac_conf.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+Filename   : platform_rv32imac_conf.h
+Author     : pry
+Date       : 24/06/2017
+Licence    : LGPL v3+; see COPYING for details.
+Description: The configuration file for RV32IMAC HAL.
+******************************************************************************/
+
+/* Config Includes ***********************************************************/
+#include "Platform/RV32IMAC/Chips/FE310-G000/platform_FE310-G000.h"
+/* End Config Includes *******************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
