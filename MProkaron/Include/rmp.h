a952e66810f69b1f48cf2af5fd17c20ffb1a0b97
EDI Systems <644580703@qq.com>
2018-07-19 20:16:51 -0400

    :fire:clean up files

---
+++ MProkaron/Include/rmp.h
@@ -0,0 +1,34 @@
+/******************************************************************************
+Filename    : rmp.h
+Author      : pry
+Date        : 01/04/2017
+Licence     : LGPL v3+; see COPYING for details.
+Description : The header of the RMP RTOS. This header defines the error codes,
+              operation flags and system call numbers in a generic way.
+******************************************************************************/
+
+/* Defines *******************************************************************/
+#ifndef __RMP_H__
+#define __RMP_H__
+
+#define __HDR_DEFS__
+#include "Platform/rmp_platform.h"
+#include "Kernel/rmp_kernel.h"
+#undef __HDR_DEFS__
+
+#define __HDR_STRUCTS__
+#include "Platform/rmp_platform.h"
+#include "Kernel/rmp_kernel.h"
+#undef __HDR_STRUCTS__
+
+#define __HDR_PUBLIC_MEMBERS__
+#include "Platform/rmp_platform.h"
+#include "Kernel/rmp_kernel.h"
+#undef __HDR_PUBLIC_MEMBERS__
+
+#endif /* __RMP_H__ */
+/* End Defines ***************************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
