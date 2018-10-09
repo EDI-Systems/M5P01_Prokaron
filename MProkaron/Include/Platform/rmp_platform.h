5563a9f42cd5396c8a4d76346648682cb5707576
EDI Systems <644580703@qq.com>
2018-10-09 03:37:08 -0400

    :green_heart:fix a bug in STM32F0 benchmark example

--- MProkaron/Include/Platform/rmp_platform.h
+++ MProkaron/Include/Platform/rmp_platform.h
@@ -7,7 +7,7 @@ Description : The platform specific types for RMP.
 ******************************************************************************/
 
 /* Platform Includes *********************************************************/
-#include "Platform/CortexM_RVM/rmp_platform_cmx_rvm.h"
+#include "Platform/CortexM/rmp_platform_cmx.h"
 /* End Platform Includes *****************************************************/
 
 /* End Of File ***************************************************************/
