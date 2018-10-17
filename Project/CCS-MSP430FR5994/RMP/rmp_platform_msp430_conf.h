69bd40b68b7cfa4e4de429ad089953b369d523e1
EDI Systems <644580703@qq.com>
2018-10-17 15:35:06 -0400

    Revert ":fire:clean up files"
    This reverts commit 7e4effc3b17077c1e84c55e36fd2ffc3c380db9b.

---
+++ Project/CCS-MSP430FR5994/RMP/rmp_platform_msp430_conf.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+Filename    : rmp_platform_msp430_conf.h
+Author      : pry
+Date        : 24/06/2017
+Licence     : LGPL v3+; see COPYING for details.
+Description : The configuration file for MSP430 MCUs.
+******************************************************************************/
+
+/* Config Includes ***********************************************************/
+#include "Platform/MSP430/Chips/MSP430FR5994/rmp_platform_MSP430FR5994.h"
+/* End Config Includes *******************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
---
+++ Project/MPLAB-PIC32MZ2048EFM100/RMP.X/rmp_platform_mipsm_conf.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+Filename    : rmp_platform_mipsm_conf.h
+Author      : pry
+Date        : 24/06/2017
+Licence     : LGPL v3+; see COPYING for details.
+Description : The configuration file for MIPS M-class.
+******************************************************************************/
+
+/* Config Includes ***********************************************************/
+#include "Platform/MIPSM/Chips/PIC32MZ2048EFM100/rmp_platform_PIC32MZ2048EFM100.h"
+/* End Config Includes *******************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
