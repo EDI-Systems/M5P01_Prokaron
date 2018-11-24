16fe74e28d97af440d4962236883e46e3d2594f2
EDI Systems <644580703@qq.com>
2018-11-24 03:00:47 -0500

    :memo:update readme

--- MProkaron/Benchmark/rmp_test.h
+++ MProkaron/Benchmark/rmp_test.h
@@ -7,7 +7,7 @@ Description : The configuration file for testing program.
 ******************************************************************************/
 
 /* Config Includes ***********************************************************/
-#include "Platform/rmp_test_STM32F767IG.h"
+#include "Platform/rmp_test_HC32L136K8.h"
 /* End Config Includes *******************************************************/
 
 /* End Of File ***************************************************************/
