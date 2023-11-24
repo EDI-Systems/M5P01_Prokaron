2fdd9cfd726e6ac0f3e7e2db0207857357eadd86
runshenghou <rshoux@gmail.com>
2023-11-24 16:05:42 +0800

    :fire: modified rmp_platform_a7m_gcc.s

--- Source/Platform/A7M/rmp_platform_a7m_gcc.s
+++ Source/Platform/A7M/rmp_platform_a7m_gcc.s
@@ -2,8 +2,7 @@
 Filename    : rmp_platform_a7m_asm_gcc.s
 Author      : songleifeng
 Date        : 07/19/2018
-Description : The assembly part of the RMP RTOS. This is for Cortex-M3/4/7, and
-              is used on gcc. This is derived from the MDK version.
+Description : The assembly part of the RMP RTOS. This is for ARMv7-M.
 ******************************************************************************/
 
 /* The ARM Cortex-M Architecture **********************************************
@@ -16,35 +15,47 @@ IPSR                         Interrupt Program Status Register.
 APSR                         Application Program Status Register.
 EPSR                         Execute Program Status Register.
 The above 3 registers are saved into the stack in combination(xPSR).
+
 The ARM Cortex-M4/7 also include a FPU.
 ******************************************************************************/
+
+/* Begin Import **************************************************************/
+    /* The real task switch handling function */
+    .global             _RMP_Run_High
+    /* The real systick handler function */
+    .global             _RMP_Tim_Handler
+    /* The PID of the current thread */
+    .global             RMP_Thd_Cur
+    /* The stack address of current thread */
+    .global             RMP_SP_Cur
+/* End Import ****************************************************************/
             
+/* Begin Export **************************************************************/
+    /* Disable all interrupts */
+    .global             RMP_Int_Disable
+    /* Enable all interrupts */
+    .global             RMP_Int_Enable
+    /* Mask/unmask some interrupts */
+    .global             RMP_Int_Mask
+    /* Get the MSB/LSB */
+    .global             _RMP_A7M_MSB_Get
+    .global             _RMP_A7M_LSB_Get
+    /* Start the first thread */
+    .global             _RMP_Start
+    /* The PendSV trigger */
+    .global             _RMP_Yield
+    /* The system pending service routine */
+    .global             PendSV_Handler
+    /* The systick timer routine */
+    .global             SysTick_Handler
+/* End Export ****************************************************************/
+
 /* Begin Header **************************************************************/
+    .section            ".text.arch"
+    .align              3
     .syntax             unified
     .thumb
-    .section            ".text"
-    .align              3
 /* End Header ****************************************************************/
-
-/* Begin Exports *************************************************************/
-    .global             RMP_Int_Disable       
-    .global             RMP_Int_Enable
-    .global             RMP_Int_Mask     
-    .global             RMP_MSB_Get
-    .global             _RMP_Start
-    .global             _RMP_Yield        
-    .global             PendSV_Handler 
-    .global             SysTick_Handler                               
-/* End Exports ***************************************************************/
-
-/* Begin Imports *************************************************************/
-    .extern             _RMP_Run_High 
-    .extern             _RMP_Tick_Handler     
-    .extern             RMP_Thd_Cur
-    .extern             RMP_SP_Cur        
-    .extern             RMP_Ctx_Save
-    .extern             RMP_Ctx_Load
-/* End Imports ***************************************************************/
 
 /* Begin Function:RMP_Int_Disable *********************************************
 Description : The function for disabling all interrupts. Does not allow nesting.
@@ -58,7 +69,7 @@ RMP_Int_Disable:
     .cantunwind
     CPSID               I                                                       
     BX                  LR       
-    .fnend				
+    .fnend
 /* End Function:RMP_Int_Disable **********************************************/
 
 /* Begin Function:RMP_Int_Enable **********************************************
@@ -67,7 +78,7 @@ Input       : None.
 Output      : None.
 Return      : None.
 ******************************************************************************/
-.thumb_func
+    .thumb_func
 RMP_Int_Enable:
     .fnstart
     .cantunwind
@@ -86,19 +97,21 @@ Return      : None.
 RMP_Int_Mask:
     .fnstart
     .cantunwind
-    MSR                 BASEPRI,R0                                                        
+    MSR                 BASEPRI,R0
+    /* We are not influenced by errata #837070 as the next */
+    /* instruction is BX LR. Thus we have a free window. */
     BX                  LR
     .fnend
 /* End Function:RMP_Int_Mask *************************************************/
 
-/* Begin Function:RMP_MSB_Get *************************************************
+/* Begin Function:_RMP_A7M_MSB_Get ********************************************
 Description : Get the MSB of the word.
 Input       : rmp_ptr_t R0 - The value.
 Output      : None.
-Return      : ptr_t - The MSB position.
+Return      : rmp_ptr_t R0 - The MSB position.
 ******************************************************************************/
     .thumb_func
-RMP_MSB_Get:
+_RMP_A7M_MSB_Get:
     .fnstart
     .cantunwind
     CLZ                 R1,R0
@@ -106,7 +119,23 @@ RMP_MSB_Get:
     SUBS                R0,R1
     BX                  LR
     .fnend
-/* End Function:RMP_MSB_Get **************************************************/
+/* End Function:_RMP_A7M_MSB_Get *********************************************/
+
+/* Begin Function:_RMP_A7M_LSB_Get ********************************************
+Description : Get the LSB of the word.
+Input       : rmp_ptr_t R0 - The value.
+Output      : None.
+Return      : rmp_ptr_t R0 - The LSB position.
+******************************************************************************/
+    .thumb_func
+_RMP_A7M_LSB_Get:
+    .fnstart
+    .cantunwind
+    RBIT                R0,R0
+    CLZ                 R0,R0
+    BX                  LR
+    .fnend
+/* End Function:_RMP_A7M_LSB_Get *********************************************/
 
 /* Begin Function:_RMP_Yield **************************************************
 Description : Trigger a yield to another thread.
@@ -118,10 +147,9 @@ Return      : None.
 _RMP_Yield:
     .fnstart
     .cantunwind
-    LDR                 R0,=0xE000ED04        
-    LDR                 R1,=0x10000000        
+    LDR                 R0,=0xE000ED04      /* The NVIC_INT_CTRL register */
+    LDR                 R1,=0x10000000      /* Trigger the PendSV */
     STR                 R1,[R0]
-                   
     ISB 
     BX                  LR         
     .fnend
@@ -136,14 +164,12 @@ Return      : None.
     .thumb_func
 _RMP_Start:
     .fnstart
-    .cantunwind
-    SUBS                R1,#64               
-    MSR                 PSP,R1                
-    MOVS                R4,#0x02              
+    .cantunwind           
+    MSR                 PSP,R1              /* Set the stack pointer */
+    MOVS                R4,#0x02            /* Previleged thread mode */
     MSR                 CONTROL,R4
-                                 
     ISB
-    BLX                 R0              
+    BLX                 R0                  /* Branch to our target */
     .fnend
 /* End Function:_RMP_Start ***************************************************/
 
@@ -163,28 +189,22 @@ Return      : None.
 PendSV_Handler:
     .fnstart
     .cantunwind
-    MRS                 R0,PSP
-    TST                 LR,#0x10            /* Are we using the FPU or not at all? */
-    .hword              0xBF08              /* IT EQ (.hword for compatibility with no FPU support) */
+    MRS                 R0,PSP              /* Save registers */
+    TST                 LR,#0x10            /* Save FPU registers */
+    .hword              0xBF08              /* IT EQ ;If yes, (DCI for compatibility with no FPU support) */
     .hword              0xED20              /* VSTMDBEQ R0!,{S16-S31} */
     .hword              0x8A10              /* Save FPU registers not saved by lazy stacking. */
-    STMDB               R0!,{R4-R11,LR}
+    STMDB               R0!,{R4-R11,LR}     /* Save the general purpose registers. */
     
-    BL                  RMP_Ctx_Save               
-    
-    LDR                 R1,=RMP_SP_Cur          
+    LDR                 R1,=RMP_SP_Cur      /* Save The SP to control block. */
     STR                 R0,[R1]
+    BL                  _RMP_Run_High       /* Get the highest ready task. */
+    LDR                 R1,=RMP_SP_Cur      /* Load the SP. */
+    LDR                 R0,[R1]  
     
-    BL                  _RMP_Run_High        
-    
-    LDR                 R1,=RMP_SP_Cur           
-    LDR                 R0,[R1]
-    
-    BL                  RMP_Ctx_Load             
-    
-    LDMIA               R0!,{R4-R11,LR}
-    TST                 LR,#0x10            /* Are we using the FPU or not at all? */
-    .hword              0xBF08              /* IT EQ (.hword for compatibility with no FPU support) */
+    LDMIA               R0!,{R4-R11,LR}     /* Restore registers */
+    TST                 LR,#0x10            /* Load FPU registers */
+    .hword              0xBF08              /* IT EQ ;If yes, (DCI for compatibility with no FPU support) */
     .hword              0xECB0              /* VLDMIAEQ R0!,{S16-S31} */
     .hword              0x8A10              /* Load FPU registers not loaded by lazy stacking. */
     MSR                 PSP,R0
@@ -211,8 +231,8 @@ SysTick_Handler:
     .cantunwind
     PUSH                {LR}
     
-    MOVS                R0,#0x01                    
-    BL                  _RMP_Tick_Handler
+    MOVS                R0,#0x01            /* We are not using tickless. */
+    BL                  _RMP_Tim_Handler
     
     POP                 {PC}
     NOP
