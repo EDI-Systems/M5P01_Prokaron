bc21ae7f14a83b26130e6e1442e5ab432a08bab4
edi-systems <644580703@qq.com>
2023-12-15 14:25:50 +0800

    :green_heart: fix x86 port compilation

--- Project/GCCMF-X86-LINUX/rmp_platform.h
+++ Project/GCCMF-X86-LINUX/rmp_platform.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 Filename    : rmp_platform.h
-Author      : pry 
+Author      : pry wyh
 Date        : 22/07/2017
 Licence     : LGPL v3+; see COPYING for details.
 Description : The platform specific types for RMP.
