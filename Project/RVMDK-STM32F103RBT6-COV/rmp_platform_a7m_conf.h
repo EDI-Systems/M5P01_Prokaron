4c9dc9d326b3618134ac6d4b146f86cdf3da495d
runyupan <rypan@sdu.edu.cn>
2024-05-04 19:13:47 +0800

    :fire: update testing framework

--- Project/RVMDK-STM32F103RBT6-COV/rmp_platform_a7m_conf.h
+++ Project/RVMDK-STM32F103RBT6-COV/rmp_platform_a7m_conf.h
@@ -7,7 +7,7 @@ Description : The configuration file for ARMv7-M HAL.
 ******************************************************************************/
 
 /* Config Includes ***********************************************************/
-#include "Platform/A7M/Chip/STM32F103RB/rmp_platform_stm32f103rb.h"
+#include "rmp_platform_stm32f103rb.h"
 /* End Config Includes *******************************************************/
 
 /* End Of File ***************************************************************/
