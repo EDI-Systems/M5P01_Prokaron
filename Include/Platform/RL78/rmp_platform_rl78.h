30be30f8b7ca55f76a6872ca2203da47d797ec5f
Twisted <73338797+lht20020816@users.noreply.github.com>
2024-06-03 15:00:22 +0800

    Update rmp_platform_rl78.h
    Signed-off-by: Twisted <73338797+lht20020816@users.noreply.github.com>

--- Include/Platform/RL78/rmp_platform_rl78.h
+++ Include/Platform/RL78/rmp_platform_rl78.h
@@ -67,8 +67,11 @@ typedef rmp_s16_t rmp_ret_t;
 /* System macros *************************************************************/
 /* Compiler "extern" keyword setting */
 #define EXTERN                          extern
-/* Full descending stack of rmp_ptr_t, word-addressing with 1-word alignment */
+/* The order of bits in one CPU machine word */
 #define RMP_WORD_ORDER                  (4U)
+/* The maximum length of char printing */
+#define RMP_DEBUG_PRINT_MAX             (255U)
+/* Full descending stack of rmp_ptr_t, word-addressing with 1-word alignment */
 #define RMP_STACK_TYPE                  RMP_STACK_FULL_DESCEND
 #define RMP_STACK_ALIGN                 (1U)
 #define RMP_STACK_ELEM                  rmp_ptr_t
