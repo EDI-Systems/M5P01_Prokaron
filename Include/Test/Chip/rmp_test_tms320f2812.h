bdf03aa619b2860fdad20ae1d58279637e29e589
Runyu Pan <rypan@rtospro.com>
2025-03-05 03:46:11 +0800

    :arrow_up: add XINTF2 memory test for C2812

--- Include/Test/Chip/rmp_test_tms320f2812.h
+++ Include/Test/Chip/rmp_test_tms320f2812.h
@@ -38,6 +38,8 @@ ISR Blocking message queue         : 727 / 843 / 727
 #define TEST_MEM_POOL           (2048U)
 /* Minimal build switch */
 /* #define MINIMAL_SIZE */
+/* TEST_EXIT used as XINTF2 512K x 16 55ns SRAM test switch */
+#define TEST_EXIT
 /* Timestamp data type */
 typedef rmp_u16_t rmp_tim_t;
 /* End Define ****************************************************************/
@@ -70,6 +72,24 @@ void Timer_Init(void)
     CpuTimer1Regs.TPRH.all=0U;
     CpuTimer1Regs.TCR.bit.TRB=1U;
     CpuTimer1Regs.TCR.bit.TSS=0U;
+
+#ifdef TEST_EXIT
+    /* XTIMCLK = XTIMCLK2 = SYSCLKOUT, up to three writes buffered */
+    XintfRegs.XINTCNF2.bit.CLKMODE=0U;
+    XintfRegs.XINTCNF2.bit.CLKOFF=1U;
+    XintfRegs.XINTCNF2.bit.WRBUFF=3U;
+    XintfRegs.XTIMING2.bit.X2TIMING=0U;
+    XintfRegs.XTIMING2.bit.USEREADY=0U;
+
+    /* Parameters below are quite stringent, pushing 55ns SRAM to 20ns;
+     * they should do when powered with 3.3V, and relax if issues occur */
+    XintfRegs.XTIMING2.bit.XRDLEAD=1;
+    XintfRegs.XTIMING2.bit.XRDACTIVE=2;
+    XintfRegs.XTIMING2.bit.XRDTRAIL=0;
+    XintfRegs.XTIMING2.bit.XWRLEAD=1;
+    XintfRegs.XTIMING2.bit.XWRACTIVE=2;
+    XintfRegs.XTIMING2.bit.XWRTRAIL=0;
+#endif
 }
 /* End Function:Timer_Init ***************************************************/
 
@@ -106,8 +126,66 @@ void Int_Disable(void)
 {
     CpuTimer1Regs.TCR.bit.TIE=0U;
 }
-#endif
 /* End Function:Int_Disable **************************************************/
+
+/* Function:Test_Exit *********************************************************
+Description : Exit the test.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+#ifdef TEST_EXIT
+void Test_Exit(void)
+{
+    rmp_ptr_t Count;
+    rmp_tim_t Start;
+    rmp_tim_t End;
+    rmp_tim_t Total;
+    volatile rmp_u16_t* Ptr;
+
+    /* Perform XINTF2 test here */
+    Ptr=(rmp_u16_t*)(0x00080000U);
+
+    RMP_DBG_S("XINTF2 SRAM");
+    for(Count=0U;Count<24U;Count++)
+    {
+        RMP_DBG_S(" ");
+    }
+    RMP_DBG_S(": ");
+
+    /* Write test data */
+    for(Count=0U;Count<524288UL;Count++)
+    {
+        Ptr[Count]=(rmp_u16_t)Count;
+    }
+    /* Read back to see whether we're good */
+    for(Count=0U;Count<524288UL;Count++)
+    {
+        if(Ptr[Count]!=(rmp_u16_t)Count)
+        {
+            RMP_DBG_S("failed!\r\n");
+            while(1);
+        }
+    }
+
+    /* How long does it take to write 1K data? - usually 8000 cycles */
+    Start=RMP_CNT_READ();
+#pragma UNROLL(64)
+#pragma MUST_ITERATE(1024)
+    for(Count=0U;Count<1024;Count++)
+    {
+        Ptr[Count]=(rmp_u16_t)Count;
+    }
+    End=RMP_CNT_READ();
+    Total=End-Start;
+    RMP_DBG_I(Total);
+    RMP_DBG_S(" / 1K words\r\n");
+
+    while(1);
+}
+#endif
+#endif
+/* End Function:Test_Exit ****************************************************/
 
 /* End Of File ***************************************************************/
 
